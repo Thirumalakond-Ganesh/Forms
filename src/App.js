7b40bd8370fdbff9d81c87957d725dca8356b36b
Thirumalakonda-Ganesh <ganitthirumala50@gmail.com>
2023-12-01 11:37:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import * as React from "react"
+import {useForm} from "react-hook-form";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App(){
+  const {register, handleSubmit,formState: { errors }} = useForm();
+  const onSubmit=(data)=>console.log(data)
+ 
 
-export default App;
+   return(
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>First Name</label>
+      <input {...register("firstname",{required:true,maxLength:20,pattern:/^[A-Za-z]+$/i})} />
+      {errors?.firstname?.type==="required"&&<p>this field is required</p>}
+      {errors?.firstname?.type==="maxLength"&&(<p>First Name Cannot Exceed 20 charecters</p>)}
+      {errors?.firstname?.type==="pattern"&&(<p>Alphabetical charecters only</p>)}
+      <label>Last Name</label>
+      <input {...register("lastname",{pattern:/^[A-Za-z]+$/i})} />
+      {errors?.lastname?.type==="pattern"&&(<p>Alphabetical charecters only</p>)}
+      <label>Age</label>
+      <input {...register("age",{min:18, max:99})} />
+      {errors.age&&(<p>You Must be older then 18 and younger then 99 years old</p>)}
+
+      <input type="submit"/>
+
+    </form>
+   );
+
+}
